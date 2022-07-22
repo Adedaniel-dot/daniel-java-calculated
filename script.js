cde8fe162fa52b09af671967eb9832d7c853db8e
Adedaniel-dot <105417493+Adedaniel-dot@users.noreply.github.com>
2022-07-22 13:19:29 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var opr = prompt("operator: ")
+var num1 =parseInt(prompt("NUMBER 1: "));
+var num2 =parseInt(prompt("NUMBER 2: "));
+
+ if(opr == "+")
+ {
+     console.log(num1 + num2)
+ }
+ else if(opr == "-")
+ {
+     console.log(num1 - num2)   
+ }
+ else if(opr == "*")
+ {
+     console.log(num1 * num2)
+ }
+ else if(opr == "/")
+ {
+     console.log(num1 / num2)
+ } 
